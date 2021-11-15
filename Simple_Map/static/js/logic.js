40835487f6bf53f27ed4b5f9d43d6efaa7bc777b
Salman Razzaq <muhammad.razzaq2@studio.unibo.it>
2021-11-14 22:57:43 -0500

    Mapping_Multiple_Points Markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,10 +5,10 @@ let cityData = cities;
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -31,3 +31,23 @@ cityData.forEach(function(city) {
 //     color: "black",
 //     fillColor: '#ffffa1'
 // }).addTo(map);
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [37.6213, -122.3790],
+    [30.1974292, -97.6663058],
+    [43.687324, -79.621622],
+    [40.641766, -73.780968]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    opacity: 0.5,
+    weight: '4',
+    dashArray: '20, 20',
+    dashOffset: '0'
+
+
+}).addTo(map);
