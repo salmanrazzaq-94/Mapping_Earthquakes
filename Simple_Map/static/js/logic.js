fcdf7636d3cbefbaf7e84be14ca6ec8182c1f81a
Salman Razzaq <muhammad.razzaq2@studio.unibo.it>
2021-11-16 22:10:17 -0500

    Mapping GeoJSON data

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,7 +5,43 @@ let cityData = cities;
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+// Add GeoJSON data.
+let sanFranAirport = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": "3469",
+            "name": "San Francisco International Airport",
+            "city": "San Francisco",
+            "country": "United States",
+            "faa": "SFO",
+            "icao": "KSFO",
+            "alt": "13",
+            "tz-offset": "-8",
+            "dst": "A",
+            "tz": "America/Los_Angeles"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.375, 37.61899948120117]
+        }
+    }]
+};
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+            .bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,13 +53,13 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, { radius: city.population / 200000, color: "orange" })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-        .addTo(map);
-});
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, { radius: city.population / 200000, color: "orange" })
+//         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//         .addTo(map);
+// });
 
 // //  Add a marker to the map for Los Angeles, California.
 // let marker = L.circleMarker([34.0522, -118.2437], {
@@ -34,20 +70,20 @@ cityData.forEach(function(city) {
 
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-    [37.6213, -122.3790],
-    [30.1974292, -97.6663058],
-    [43.687324, -79.621622],
-    [40.641766, -73.780968]
-];
+// let line = [
+//     [37.6213, -122.3790],
+//     [30.1974292, -97.6663058],
+//     [43.687324, -79.621622],
+//     [40.641766, -73.780968]
+// ];
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    opacity: 0.5,
-    weight: '4',
-    dashArray: '20, 20',
-    dashOffset: '0'
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "blue",
+//     opacity: 0.5,
+//     weight: '4',
+//     dashArray: '20, 20',
+//     dashOffset: '0'
 
 
-}).addTo(map);
+// }).addTo(map);
