c4b173a83f9f36b75f32f97fd9b5bdf269b0afac
Salman Razzaq <muhammad.razzaq2@studio.unibo.it>
2021-11-20 22:48:09 -0500

    adding components

--- Simple_Map/static/js/logicStep1.js
+++ Simple_Map/static/js/logicStep1.js
@@ -23,37 +23,41 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    'Streets': streets,
-    'Satellite Streets': satelliteStreets
+    "Streets": streets,
+    "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/salmanrazzaq-94/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Simple_Map/torontoNeighborhoods.json";
-
-
-// Create a style for the lines.
-let myStyle = {
-    color: "#0000FF",
-    weight: 1
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data, {
-        style: myStyle
-
-
-    }).addTo(map);
+    L.geoJson(data).addTo(map);
 });
+
+
+
+// // Create a style for the lines.
+// let myStyle = {
+//     color: "#0000FF",
+//     weight: 1
+// }
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJson(data, {
+//         style: myStyle
+
+
+//     }).addTo(map);
+// });
